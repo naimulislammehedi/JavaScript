ce846b3d42058a3ea6dc33984e90b06ebba1e71d
Mehedi <mehedi.interface@gmail.com>
2025-09-18 10:28:50 +0300

    added exercise 5 and 6

---
+++ Module 1 - Basics of JavaScript/Exercise_5/script.js
@@ -0,0 +1,10 @@
+
+let person = {
+    name: "Mehedi", 
+    age: 25, 
+    city: "Doha"
+}; 
+
+console.log(person.name); 
+console.log(person.age); 
+console.log(person.city)
---
+++ Module 1 - Basics of JavaScript/Exercise_6/script.js
@@ -0,0 +1,5 @@
+let number = 20; 
+let oddEven = (number % 2 == 0) ? "Even" : "Odd"; 
+console.log(oddEven); 
+
+
